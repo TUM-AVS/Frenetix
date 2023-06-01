898c2bc03a49df40d68350ba914eb82c1b84b11e
Georg Schmalhofer <ga45foz@mytum.de>
2023-06-01 14:58:43 +0200

    typing errors fixed

--- setup_script1.sh
+++ setup_script1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-git -C /app/thirdParty/commonroad-driveability-checker submodule update --init
-mkdir /app/thirdParty/commonroad-driveability-checker/build_debug && cd /app/thirdParty/commonroad-driveability-checker/build_debug
+git -C /app/thirdParty/commonroad-drivability-checker submodule update --init
+mkdir /app/thirdParty/commonroad-drivability-checker/build_debug && cd /app/thirdParty/commonroad-drivability-checker/build_debug
 
 cmake -DCMAKE_INSTALL_PREFIX=/app/thirdParty/driveabilityChecker -DCMAKE_BUILD_TYPE=Release ..
 cmake --build . -- -j 1
