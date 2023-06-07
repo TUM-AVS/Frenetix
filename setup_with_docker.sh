4c74bd56a5d1c83e3eb37d18a177b3560cebf8bf
Georg Schmalhofer <ga45foz@mytum.de>
2023-06-07 16:14:44 +0200

    to make setup_with_docker.sh also work with ci runner

--- setup_with_docker.sh
+++ setup_with_docker.sh
@@ -17,7 +17,7 @@ if [ "$(docker ps -aq -f name=$CONTAINER_NAME)" ]; then
   docker rm $CONTAINER_NAME
 fi
 
-docker run -td -v ./src:/app/src -v ./setup_wheel.sh:/app/setup_wheel.sh -v ./setup.py:/app/setup.py --name $CONTAINER_NAME $IMAGE_NAME 
+docker run -td -v $(pwd)/src:/app/src -v $(pwd)/setup_wheel.sh:/app/setup_wheel.sh -v $(pwd)/setup.py:/app/setup.py --name $CONTAINER_NAME $IMAGE_NAME 
 docker exec $CONTAINER_NAME /app/setup_wheel.sh
 docker cp $CONTAINER_NAME:/app/dist/ .
 docker stop $CONTAINER_NAME
