19e0b1b59d5a58d8f087b656639850d262313495
Georg Schmalhofer <ga45foz@mytum.de>
2023-06-07 08:14:27 +0200

    improving ci performance

--- setup_script1.sh
+++ setup_script1.sh
@@ -3,5 +3,5 @@ git -C /app/thirdParty/commonroad-drivability-checker submodule update --init
 mkdir /app/thirdParty/commonroad-drivability-checker/build_debug && cd /app/thirdParty/commonroad-drivability-checker/build_debug
 
 cmake -DCMAKE_INSTALL_PREFIX=/app/thirdParty/driveabilityChecker -DCMAKE_BUILD_TYPE=Release ..
-cmake --build . -- -j 1
+cmake --build . -- -j $(nproc)
 cmake --install .
