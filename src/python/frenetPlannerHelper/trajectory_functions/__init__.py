189e33e374e94c84f3c52be8735f835976280176
Tobias Markus <tobias.markus@tum.de>
2023-08-01 19:01:53 +0200

    Remove leftover debugging statement

--- src/python/frenetPlannerHelper/trajectory_functions/__init__.py
+++ src/python/frenetPlannerHelper/trajectory_functions/__init__.py
@@ -3,7 +3,6 @@ from .._frenetPlannerHelper import trajectory_functions
 __all__ = []
 
 for name in dir(trajectory_functions):
-    print(name)
     globals().update({name: getattr(trajectory_functions, name)})
     __all__.append(name)
 
