61a46f2f553cd35aa9b4e6a10a392b0606adf6bc
Tobias Markus <tobias.markus@tum.de>
2023-11-10 14:27:12 +0100

    Use norm for VelocityOffsetCost

--- src/strategies/CostFunctions/CalculateVelocityOffsetCost.cpp
+++ src/strategies/CostFunctions/CalculateVelocityOffsetCost.cpp
@@ -18,11 +18,9 @@ void CalculateVelocityOffsetCost::evaluateTrajectory(TrajectorySample& trajector
 
     const Eigen::VectorXd& vel = trajectory.m_cartesianSample.velocity;
 
-    int half_idx = vel.size() / 2;
-    int half_count = vel.size() - half_idx - 1;
-    cost = (vel.segment(half_idx, half_count) - Eigen::VectorXd::Constant(half_count, m_desiredSpeed)).cwiseAbs().sum();
-    cost += std::pow((vel(vel.size()-1) - m_desiredSpeed), 2);
+    cost = (vel.tail(vel.size() / 2).array() - m_desiredSpeed).matrix().norm();
 
+    cost += std::pow((vel(vel.size()-1) - m_desiredSpeed), 2);
 
     trajectory.addCostValueToList(m_functionName, cost, cost*m_costWeight);
 }
