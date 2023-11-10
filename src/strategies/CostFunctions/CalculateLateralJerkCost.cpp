f2ffac9ad545f1cea44936e89895d25440e6d2b4
Tobias Markus <tobias.markus@tum.de>
2023-11-10 14:23:55 +0100

    Use sqrt for jerk costs

--- src/strategies/CostFunctions/CalculateLateralJerkCost.cpp
+++ src/strategies/CostFunctions/CalculateLateralJerkCost.cpp
@@ -13,7 +13,7 @@ CalculateLateralJerkCost::CalculateLateralJerkCost(std::string funName, double c
 void CalculateLateralJerkCost::evaluateTrajectory(TrajectorySample& trajectory)
 {
     double cost {0};
-    cost = trajectory.m_trajectoryLateral.squaredJerkIntegral(trajectory.m_dT);
+    cost = std::sqrt(trajectory.m_trajectoryLateral.squaredJerkIntegral(trajectory.m_dT));
 
     trajectory.addCostValueToList(m_functionName, cost, cost*m_costWeight);
 }
