5326182523ac0ba6542a9f8b044cfa707b1d0224
Tobias Markus <tobias.markus@tum.de>
2023-11-10 14:23:30 +0100

    Refactor DistanceToObstacleCost

--- src/strategies/CostFunctions/CalculateDistanceToObstacleCost.cpp
+++ src/strategies/CostFunctions/CalculateDistanceToObstacleCost.cpp
@@ -18,9 +18,16 @@ void CalculateDistanceToObstacleCost::evaluateTrajectory(TrajectorySample& traje
     posMatrix.row(1) = trajectory.m_cartesianSample.y;
 
     for (int i = 0; i < m_obstacles.rows(); ++i) {
-        Eigen::Vector2d obstaclePos(m_obstacles(i, 0), m_obstacles(i, 1));
-        Eigen::MatrixXd diffMatrix = (posMatrix.colwise() - obstaclePos).colwise().norm();
-        cost += (1.0 / (diffMatrix.array() * diffMatrix.array())).sum();
+        Eigen::Vector2d obstaclePos = m_obstacles.row(i);
+
+        Eigen::MatrixXd diffMatrix = (posMatrix.colwise() - obstaclePos).colwise().squaredNorm();
+
+        const bool use_minimum = true;
+        if (use_minimum) {
+            cost += 1.0 / diffMatrix.minCoeff();
+        } else {
+            cost += (1.0 / diffMatrix.array()).sum();
+        }
     }
 
     trajectory.addCostValueToList(m_functionName, cost, cost*m_costWeight);
