e00c0bfa3131014ed5d653c3269ae3cc30b342ad
Tobias Markus <tobias.markus@tum.de>
2024-01-26 13:59:41 +0100

    Use newer CCS version

--- cmake/external/ExternalDrivabilityChecker.cmake
+++ cmake/external/ExternalDrivabilityChecker.cmake
@@ -5,7 +5,11 @@ find_package(Threads REQUIRED)
 # Required for LINK_LIBRARIES_ONLY_TARGETS (gtest links directly to pthread)
 add_library(pthread ALIAS Threads::Threads)
 
-set(_crdc_local ON)
+set(_crdc_local OFF)
+
+if(DEFINED ENV{CIBUILDWHEEL})
+    set(_crdc_local ON)
+endif()
 
 if(_crdc_local)
     add_subdirectory(${PROJECT_SOURCE_DIR}/third_party/crdc)
@@ -15,7 +19,7 @@ else()
     FetchContent_Declare(
         crdc
         GIT_REPOSITORY git@gitlab.lrz.de:motionplanning1/commonroad-drivability-checker.git
-        GIT_TAG        wip-experimental-fallible-conversion
+        GIT_TAG        feature/projection-domain-check-perf
     )
 
     FetchContent_MakeAvailable(crdc)
