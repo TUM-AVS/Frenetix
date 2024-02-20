9ad8eb0e45c2c2933237d8169890373cea9d222b
Tobias Markus <tobias.markus@tum.de>
2024-02-20 13:35:22 +0100

    Improve correlation coefficient check

--- src/math/mvn.cpp
+++ src/math/mvn.cpp
@@ -8,6 +8,23 @@
 
 #include "covariance.hpp"
 
+
+template<int Var1, int Var2, typename Scalar, int Rows, int Cols>
+static inline Scalar pearson_correlation(
+    const Eigen::Matrix<Scalar, Rows, Cols>& covar,
+    const typename Eigen::NumTraits<Scalar>::Real& prec = Eigen::NumTraits<Scalar>::dummy_precision()
+) {
+    Scalar rho = covar(Var1, Var2) / std::sqrt(covar.diagonal()(Var1) * covar.diagonal()(Var2));
+
+    auto one = static_cast<Scalar>(1.0);
+
+    if (rho + prec < -one || rho - prec > one) {
+        throw std::domain_error { "invalid correlation coefficient" };
+    }
+
+    return rho;
+}
+
 double bvn_prob(
     const Eigen::AlignedBox<double, 2>& box,
     const Eigen::Matrix<double, 2, 1>& means,
@@ -22,13 +39,7 @@ double bvn_prob(
 
     Eigen::AlignedBox2d norm_box { norm_lower, norm_upper };
 
-    double rho = covar(0, 1) / std::sqrt(covar(0, 0) * covar(1, 1));
-
-    assert(!std::isnan(rho));
-    if (rho < -1.0 || rho > 1.0) {
-        throw std::domain_error { "invalid correlation coefficient" };
-    }
-    assert(rho >= -1.0 && rho <= 1.0);
+    double rho = pearson_correlation<0, 1>(covar);
 
     const int32_t infin[2] = { 2, 2 };
     double result = bvnmvn_(norm_box.min().data(), norm_box.max().data(), &infin[0], &rho);
