2824707dfd2d864d04a18ae417267acbed52ddfd
Tobias Markus <tobias.markus@tum.de>
2024-03-11 16:27:22 +0100

    Remove system property in CCS binding

--- src/python/frenetix/__init__.pyi
+++ src/python/frenetix/__init__.pyi
@@ -1,17 +1,13 @@
-from . import _frenetix as _frenetix
-from ._frenetix import trajectory_functions as trajectory_functions
-from frenetix._frenetix import CartesianSample as CartesianSample, CoordinateSystemWrapper as CoordinateSystemWrapper, CurviLinearSample as CurviLinearSample, PoseWithCovariance as PoseWithCovariance, PredictedObject as PredictedObject, QuarticTrajectory as QuarticTrajectory, QuinticTrajectory as QuinticTrajectory, TrajectoryHandler as TrajectoryHandler, TrajectorySample as TrajectorySample
-
-__all__ = ['CartesianSample', 'CoordinateSystemWrapper', 'CurviLinearSample', 'PoseWithCovariance', 'PredictedObject', 'QuarticTrajectory', 'QuinticTrajectory', 'TrajectoryHandler', 'TrajectorySample', 'trajectory_functions']
-
-# Names in __all__ with no definition:
-#   CartesianSample
-#   CoordinateSystemWrapper
-#   CurviLinearSample
-#   PoseWithCovariance
-#   PredictedObject
-#   QuarticTrajectory
-#   QuinticTrajectory
-#   TrajectoryHandler
-#   TrajectorySample
-#   trajectory_functions
+from __future__ import annotations
+from frenetix._frenetix import CartesianSample
+from frenetix._frenetix import CoordinateSystemWrapper
+from frenetix._frenetix import CurviLinearSample
+from frenetix._frenetix import PoseWithCovariance
+from frenetix._frenetix import PredictedObject
+from frenetix._frenetix import QuarticTrajectory
+from frenetix._frenetix import QuinticTrajectory
+from frenetix._frenetix import TrajectoryHandler
+from frenetix._frenetix import TrajectorySample
+from frenetix._frenetix import trajectory_functions
+from . import _frenetix
+__all__: tuple = ('CartesianSample', 'CoordinateSystemWrapper', 'CurviLinearSample', 'PoseWithCovariance', 'PredictedObject', 'QuarticTrajectory', 'QuinticTrajectory', 'TrajectoryHandler', 'TrajectorySample', 'trajectory_functions')
