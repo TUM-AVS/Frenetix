ffd731b010bf077981d9b983d5bb5a32045e2dba
Tobias Markus <tobias.markus@tum.de>
2024-05-11 10:28:11 +0200

    cmake: Update taskflow version

--- cmake/external/ExternalTaskflow.cmake
+++ cmake/external/ExternalTaskflow.cmake
@@ -1,13 +1,15 @@
 include(FetchContent)
 include(utils/FetchContentHelper)
 
+set(taskflow_version 3.7.0)
+
 FetchContent_Declare_Fallback(
     taskflow
 
     SYSTEM
 
-    URL "https://github.com/taskflow/taskflow/archive/refs/tags/v3.6.0.tar.gz"
-    URL_HASH SHA256=5a1cd9cf89f93a97fcace58fd73ed2fc8ee2053bcb43e047acb6bc121c3edf4c
+    URL "https://github.com/taskflow/taskflow/archive/refs/tags/v${taskflow_version}.tar.gz"
+    URL_HASH SHA256=788b88093fb3788329ebbf7c7ee05d1f8960d974985a301798df01e77e04233b
 
     #GIT_REPOSITORY "https://github.com/taskflow/taskflow.git"
     #GIT_TAG 12f8bd4e970ab27fd3dee3bffa24b5b48b54ba39
